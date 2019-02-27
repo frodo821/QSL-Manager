98be484c77ca07a83e1ea250cbbf8454a0e94635
frodo821 <sakaic2003@gmail.com>
2019-02-27 20:44:02 +0900

    Initialization is now exclusive,

--- src/datastore/sync.ts
+++ src/datastore/sync.ts
@@ -13,6 +13,8 @@ const TYPE_DICT = {
   removed: "RemoveQSL"
 }
 
+let initializing = false;
+
 let FS: {
   application: App;
   database: firebase.firestore.Firestore;
@@ -30,38 +32,51 @@ export function getSyncId() {
 }
 
 export async function initialize(appl: App, roomId?: string) {
-  let app: firebase.app.App | undefined;
-  if(!(app = firebase.apps.find(it=>it.name===APP_NAME)))
-    app = initializeApp(config, APP_NAME)
+  if(initializing) return;
+  initializing = true;
 
-  let database = firebase.firestore(app);
-  database.enablePersistence();
+  let app: firebase.app.App | undefined;
+  let database;
+  if(!(app = firebase.apps.find(it=>it.name===APP_NAME))) {
+    app = initializeApp(config, APP_NAME);
+    database = firebase.firestore(app);
+    database.enablePersistence();
+  } else {
+    database = firebase.firestore(app);
+  }
 
   let col: firebase.firestore.DocumentReference;
   if(roomId) {
     col = database.collection('syncRooms').doc(roomId);
     let doc = await col.get();
     if(!doc.exists) {
+      let content = `Could not sync via cloud: unknown sync room ID '${roomId}'`;
       appl.setState({
         input_msg: {
-          content: `Could not sync via cloud: unknown sync room ID '${roomId}'`,
+          content,
           type: "error"
         }
       });
-      location.hash = "";
-      location.reload();
-      return;
+      throw content;
     }
   } else {
     col = await database.collection('syncRooms').add({});
   }
 
   let room = col.collection('qsls');
+
+  FS = {
+    database,
+    application: appl,
+    room,
+    connected: true
+  };
+
   room.onSnapshot({
     next: snapshot => {
       snapshot.docChanges().map(it=>{
-        let qsl = Object.assign(it.doc.data(), {my: it.doc.id.replace('.', '/')});
-        let index = findQSL(it.doc.id.replace('.', '/'));
+        let qsl = Object.assign(it.doc.data(), {id: it.doc.id});
+        let index = findQSL(qsl.my);
         try{
           appl.props.dispatchMessage({
             type: TYPE_DICT[it.type],
@@ -76,13 +91,6 @@ export async function initialize(appl: App, roomId?: string) {
     },
     error: console.error
   });
-
-  FS = {
-    database,
-    application: appl,
-    room,
-    connected: true
-  };
 
   location.hash = `#sync=${getSyncId()}`;
   await uploadAll();
@@ -103,17 +111,19 @@ export async function uploadAll() {
   for(let qsl = 0; qsl < FS.application.props.qsls.length; qsl++) {
     let tqsl = FS.application.props.qsls[qsl];
     let {id, ...other} = tqsl;
+    (other as any).date = other.date.valueOf();
     let md;
     if(id) {
       md = FS.room.doc(id);
+      if(await updatable(tqsl, md))
+        await batcher.set(md, other);
     } else {
-      md = await FS.room.add({});
-      FS.application.props.editQSL(qsl, Object.assign({id: md.id}, other));
+      let id = (await FS.room.add(other)).id;
+      FS.application.props.editQSL(qsl, Object.assign({id}, other), "ONLINE_SYNC_EVENT");
     }
-    (other as any).date = other.date.valueOf();
-    if(await updatable(tqsl, md))
-      batcher.set(md, other);
   }
+  await batcher.commit();
+  console.log('All cached data uploaded');
 }
 
 export async function syncRemoteData() {
@@ -137,10 +147,14 @@ export async function uploadLocalData(msg: Message) {
     case "AddQSL":
       if(msg.qsl) {
         let {id, ...other} = msg.qsl;
-        let md = FS.room.doc(id);
-        if(msg.force || await updatable(msg.qsl, md)) {
-          (other as any).date = other.date.valueOf();
-          await md.set(other);
+        (other as any).date = other.date.valueOf();
+        if(id){
+          let md = FS.room.doc(id);
+          if(msg.force || await updatable(msg.qsl, md)) {
+            await md.set(other);
+          }
+        } else {
+          await FS.room.add(other);
         }
       }
       break;
