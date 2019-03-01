a6aa43630c8995c65e94eb4714b704e47a79ad81
frodo821 <sakaic2003@gmail.com>
2019-03-01 20:36:42 +0900

    string formatter fixed

--- src/string.ts
+++ src/string.ts
@@ -1,21 +1,26 @@
+export {}
 
-interface String {
-  format(fmt: string[] | {[key: string]: string}): string
+type sdict = {[key: string]: string};
+
+declare global {
+  interface String {
+    format: (fmt: string[] | sdict) => string
+  }
 }
 
-String.prototype.format = function(fmt: string[] | {[key: string]: string}) {
+String.prototype.format = function(fmt: string[] | sdict) {
   let cnt = 0;
   return this.replace(
     /{([a-zA-Z0-9_]*)(:[^}]*)?}/g,
     function(_: string, g1: string | undefined, g2: string | undefined) {
       let r: string | undefined;
       if(g1) {
-        if(!(r = fmt[g1])) {
+        if(!(r = (fmt as sdict)[g1])) {
           throw `KeyError: ${g1}`;
         }
         return r;
       }
-      if(!(r = fmt[cnt++])) {
+      if(!(r = (fmt as string[])[cnt++])) {
         throw `Index out of range: ${cnt-1}`;
       }
       return r;
